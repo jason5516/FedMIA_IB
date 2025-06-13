8fad764790242a379d341504aeae73ac16640420
柏翰 曾 <jason1234.tseng@gmail.com>
2025-06-13 17:06:01 +0800

    新增指令

--- run_ib_layers.sh
+++ run_ib_layers.sh
@@ -1,7 +1,7 @@
 # federated training and attack measurement calculatinging command 
 dataset=cifar100 # or [cifar100, dermnet, cifar10]
 model_name=ResNet18_IB_layer # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, ResNet18_IB_layer, alexnet]
-ib_model_layer=2 #[0,1,2,3]
+# ib_model_layer=2 #[0,1,2,3]
 
 opt=sgd
 seed=1 
@@ -13,15 +13,17 @@ local_epoch=1
 
 # iid experiment
 save_dir=log_fedmia/iid_ib
-CUDA_VISIBLE_DEVICES=1
-for ib_beta in 1e-6 
+# CUDA_VISIBLE_DEVICES=1
+for layer in 0 1 2 3 
 do
-    python main.py --seed $seed --num_users 10 --iid 1 --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $ib_model_layer \
-        --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
-        --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
-        --lr_up cosine --MIA_mode 1  --gpu 1
+    for ib_beta in 1e-6 
+    do
+        python main.py --seed $seed --num_users 10 --iid 1 --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $layer \
+            --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+            --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+            --lr_up cosine --MIA_mode 1  --gpu 1
+    done
 done
-
 # non-iid experiment
 # save_dir=log_fedmia/noniid
 # CUDA_VISIBLE_DEVICES=0
