f8a32aa0b60f739582a6f98c1c5dd666caa57dff
柏翰 曾 <jason1234.tseng@gmail.com>
2025-06-15 16:24:06 +0800

    add different layer IB training script

---
+++ run_ib_layers_0.sh
@@ -0,0 +1,45 @@
+# federated training and attack measurement calculatinging command 
+dataset=cifar100 # or [cifar100, dermnet, cifar10]
+model_name=ResNet18_IB_layer # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, ResNet18_IB_layer, alexnet]
+layer=0
+
+opt=sgd
+seed=1 
+lr=0.001
+local_epoch=1
+
+# ib_beta=0.00001
+
+
+# iid experiment
+save_dir=log_fedmia/iid_ib
+# CUDA_VISIBLE_DEVICES=1
+
+for ib_beta in 1e-8 1e-7 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1
+do
+    python main.py --seed $seed --num_users 10 --iid 1 --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $layer \
+        --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+        --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+        --lr_up cosine --MIA_mode 1  --gpu 0
+done
+# non-iid experiment
+# save_dir=log_fedmia/noniid
+# CUDA_VISIBLE_DEVICES=0
+
+
+# for bt in 10 1 0.1
+# do
+#     echo "Running with noniid beta $bt, IB beta $ib_beta"
+#     python  main.py --seed $seed --num_users 10 --iid 0 --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta\
+#     --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+#     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+#     --lr_up cosine --MIA_mode 1  --gpu 0
+# done
+
+# -m debugpy --listen 5010 --wait-for-client
+# --ib_costum $ib_beta --ib_beta $ib_beta
+
+# python -m debugpy --listen 5010 --wait-for-client main.py --seed $seed --num_users 10 --iid 0 --beta 10 --ib_costum $ib_beta --ib_beta $ib_beta\
+#     --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+#     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+#     --lr_up cosine --MIA_mode 1  --gpu 0
--- run_ib_layers_2.sh
+++ run_ib_layers_2.sh
@@ -1,11 +1,11 @@
 # federated training and attack measurement calculatinging command 
 dataset=cifar100 # or [cifar100, dermnet, cifar10]
 model_name=ResNet18_IB_layer # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, ResNet18_IB_layer, alexnet]
-# ib_model_layer=2 #[0,1,2,3]
+layer=2
 
 opt=sgd
 seed=1 
-lr=0.01
+lr=0.001
 local_epoch=1
 
 # ib_beta=0.00001
@@ -14,15 +14,13 @@ local_epoch=1
 # iid experiment
 save_dir=log_fedmia/iid_ib
 # CUDA_VISIBLE_DEVICES=1
-for layer in 0 1 2 3 
+
+for ib_beta in 1e-8 1e-7 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1
 do
-    for ib_beta in 1e-6 
-    do
-        python main.py --seed $seed --num_users 10 --iid 1 --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $layer \
-            --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
-            --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
-            --lr_up cosine --MIA_mode 1  --gpu 0
-    done
+    python main.py --seed $seed --num_users 10 --iid 1 --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $layer \
+        --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+        --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
+        --lr_up cosine --MIA_mode 1  --gpu 0
 done
 # non-iid experiment
 # save_dir=log_fedmia/noniid
