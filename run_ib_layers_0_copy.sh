7fb5080eb597ad62f8fea0621e4130b2372aa168
柏翰 曾 <jason1234.tseng@gmail.com>
2025-07-02 21:08:34 +0800

    layer 0 with dynamic training

--- run_ib_layers_0_copy.sh
+++ run_ib_layers_0_copy.sh
@@ -8,6 +8,7 @@ seed=1
 lr=0.001
 local_epoch=1
 dynamic_ib=entropy
+ib_beta=1e-6
 
 
 # ib_beta=0.00001
@@ -36,8 +37,8 @@ save_dir=log_fedmia/noniid
 for bt in 0.1 0.5 1 5 10
 do
     echo "Running with noniid beta $bt, IB beta $ib_beta"
-    python  main.py --seed $seed --num_users 10 --iid 0 --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $layer --dynamic_ib $dynamic_ib\
-    --dataset $dataset --model_name $model_name --epochs 300 --local_ep $local_epoch \
+    python main.py --seed $seed --num_users 10 --iid 0 --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer $layer --dynamic_ib $dynamic_ib\
+    --dataset $dataset --model_name $model_name --epochs 100 --local_ep $local_epoch \
     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
     --lr_up cosine --MIA_mode 1  --gpu 1
     
