4f89406b29d0d1be31f034e48958ae2ffbbbba50
柏翰 曾 <jason1234.tseng@gmail.com>
2025-07-17 22:03:36 +0800

    use ib_layer train on diff classes

--- run_ib_noniid_class_g0.sh
+++ run_ib_noniid_class_g0.sh
@@ -1,24 +1,24 @@
 # federated training and attack measurement calculatinging command 
 dataset=cifar10 # or [cifar100, dermnet, cifar10]
-model_name=ResNet18_IB_Block # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, alexnet]
+model_name=ResNet18_IB_layer # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, alexnet]
 
 opt=sgd
 seed=1 
 lr=1e-3
 local_epoch=1
 bt=0.1
-ib_beta=1e-6
+ib_beta=1e-3
 n_class=4
 # non-iid experiment
-save_dir=log_fedmia/noniid_ib/noniid
-CUDA_VISIBLE_DEVICES=0
+save_dir=log_fedmia/noniid_ib_class
+# CUDA_VISIBLE_DEVICES=0
 dynamic_ib=entropy
 
 
-for n_class in 8
+for n_class in 4 6
 do
-    echo "Running with n_class $n_class, IB beta dynamic $dynamic_ib"
-    python main.py --seed $seed --num_users 5 --iid 2 --n_classes $n_class --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta --dynamic_ib $dynamic_ib\
+    echo "Running with n_class $n_class, IB beta $ib_beta"
+    python main.py --seed $seed --num_users 5 --iid 2 --n_classes $n_class --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta --ib_model_layer 0 \
     --dataset $dataset --model_name $model_name --epochs 100 --local_ep $local_epoch \
     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
     --lr_up cosine --MIA_mode 1  --gpu 0
--- run_ib_noniid_class_g1.sh
+++ run_ib_noniid_class_g1.sh
@@ -1,6 +1,6 @@
 # federated training and attack measurement calculatinging command 
 dataset=cifar10 # or [cifar100, dermnet, cifar10]
-model_name=ResNet18_IB_Block # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, alexnet]
+model_name=ResNet18_IB_layer # or [ResNet18, ResNet18_IB, ResNet18_IB_Block, alexnet]
 
 opt=sgd
 seed=1 
@@ -18,7 +18,7 @@ dynamic_ib=entropy
 for n_class in 4
 do
     echo "Running with n_class $n_class, IB beta $ib_beta"
-    python main.py --seed $seed --num_users 5 --iid 2 --n_classes $n_class --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta --dynamic_ib $dynamic_ib\
+    python main.py --seed $seed --num_users 5 --iid 2 --n_classes $n_class --beta $bt --ib_costum $ib_beta --ib_beta $ib_beta ---ib_model_layer 0\
     --dataset $dataset --model_name $model_name --epochs 100 --local_ep $local_epoch \
     --lr $lr --batch_size 100 --optim $opt --save_dir $save_dir --log_folder_name $save_dir \
     --lr_up cosine --MIA_mode 1  --gpu 1
